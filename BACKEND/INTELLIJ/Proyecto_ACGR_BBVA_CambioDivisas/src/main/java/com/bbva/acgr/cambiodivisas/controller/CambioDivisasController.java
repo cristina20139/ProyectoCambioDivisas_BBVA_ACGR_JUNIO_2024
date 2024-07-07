866ddb7412d430f2d154ca72560b49031cde6476
Aura Cristina Garzón Rodríguez <auragarzonr@gmail.com>
2024-07-07 14:09:15 -0500

    Rebajada versión de log4j a 1.2.17 para compatibilidad con infraestructura de logging existente. Agregada configuración de logging SLF4J en CambioDivisasController. Añadido logger estático utilizando SLF4J en CambioDivisasController. Agregada configuración de log4j2 en XML para logging a consola y archivo. Agregadas instrucciones de logging con niveles debug, info, warn y error en CambioDivisasController.
    Autor: Aura Cristina Garzón Rodríguez (auragarzonr@gmail.com)
    Fecha: 7 de julio de 2024

--- BACKEND/INTELLIJ/Proyecto_ACGR_BBVA_CambioDivisas/src/main/java/com/bbva/acgr/cambiodivisas/controller/CambioDivisasController.java
+++ BACKEND/INTELLIJ/Proyecto_ACGR_BBVA_CambioDivisas/src/main/java/com/bbva/acgr/cambiodivisas/controller/CambioDivisasController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.bbva.acgr.cambiodivisas.service.CambioDivisasService;
 import reactor.core.publisher.Mono;
 
@@ -29,6 +32,9 @@ import reactor.core.publisher.Mono;
 @RestController
 public class CambioDivisasController {
 
+
+    private static final Logger logger = LoggerFactory.getLogger(CambioDivisasController.class);
+
     private final CambioDivisasService currencyService;
 
     /**
@@ -50,6 +56,10 @@ public class CambioDivisasController {
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/latest-rates")
     public Mono<CambioDivisasResponse> getLatestRates() {
+        logger.debug("Debug message");
+        logger.info("Info message");
+        logger.warn("Warning message");
+        logger.error("Error message");
         return currencyService.getLatestRates("bde53f3076204fa9b4bc3e16c3b3324c");
     }
 }
